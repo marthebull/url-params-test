e49cddc8b05d0b71054bb700c1075f080a8d391f
marthebull <marthebullpettersen@gmail.com>
2022-09-27 10:21:59 +0200

    test

---
+++ products.js
@@ -0,0 +1,12 @@
+const parameterString = window.location.search;
+const searchParameters = new URLSearchParams(parameterString);
+
+for (const parameter of searchParameters) {
+  console.log(parameter);
+}
+
+
+const newFirstName = searchParameters.get('firstName');
+const newLastName = searchParameters.get('lastName');
+
+//console.log(newFirstName, newLastName);
